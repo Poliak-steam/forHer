2b9fe4185feec2f554809b94139f1d6e01f0792a
Poliak-steam <47054153+Poliak-steam@users.noreply.github.com>
2024-02-17 01:18:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+new Swiper('.image-slider',{
+   navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev'
+   },
+   simulateTouch: false,
+   effect:'flip',
+   flipEffect:{
+      slideShadows:false
+   }
+});
+
